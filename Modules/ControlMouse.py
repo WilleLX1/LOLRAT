841bfca4d824c5e86dc78ed35023373c7067bd53
wdani <68472201+WilleLX1@users.noreply.github.com>
2023-11-25 16:55:33 +0100

    Updated MouseControl Script
    Again...

--- Modules/ControlMouse.py
+++ Modules/ControlMouse.py
@@ -5,21 +5,21 @@ import time
 def click_mouse(x, y, click_type):
     pyautogui.moveTo(x, y, duration=0)
     
-    if click_type == "left":
+    if click_type == 'left':
         pyautogui.click()
-    elif click_type == "double":
+    elif click_type == 'double':
         pyautogui.doubleClick()
-    elif click_type == "right":
+    elif click_type == 'right':
         pyautogui.rightClick()
-    elif click_type == "middle":
+    elif click_type == 'middle':
         pyautogui.middleClick()
     else:
-        print("Invalid click type. Please use 'left', 'double', 'right', or 'middle'.")
+        print('Invalid click type. Please use \'left\', \'double\', \'right\', or \'middle\'.')
         sys.exit(1)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print("Usage: python mouse.py x y click_type")
+        print('Usage: python mouse.py x y click_type')
         sys.exit(1)
 
     try:
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         y = int(sys.argv[2])
         click_type = sys.argv[3].lower()
     except ValueError:
-        print("Invalid arguments. Please provide integers for x and y coordinates.")
+        print('Invalid arguments. Please provide integers for x and y coordinates.')
         sys.exit(1)
 
     click_mouse(x, y, click_type)
